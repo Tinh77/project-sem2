84f2b8e731a4d183d0c5879a7f88d1adc0a417f5
exam <tuandpd00617@fpt.edu.vn>
2018-08-17 15:40:44 +0700

    tao-xong-seeder

---
+++ database/seeds/Categories_table_seeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Categories_table_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        \Illuminate\Support\Facades\DB::table('categories')->truncate();
+        \Illuminate\Support\Facades\DB::table('categories')->insert([
+            [
+                'name'=>'Quần áo',
+                'description'=>'',
+                'thumbnail'=>'https://bizweb.dktcdn.net/100/042/300/themes/55369/assets/menu_icon_3.png?1513314074912',
+            ],
+            [
+                'name'=>'Mũ',
+                'description'=>'1',
+                'thumbnail'=>'https://vn-live-02.slatic.net/original/19e1737914da112ed0b26c3b89b13ddd.jpg',
+            ],
+            [
+                'name'=>'Giày dép',
+                'description'=>'1',
+                'thumbnail'=>'https://sieuthikid.com/wp-content/uploads/2013/08/mua-sam-giay-dep-cho-be-voi-nhung-meo-cuc-doc-0.jpg',
+            ],
+            [
+                'name'=>'Đồ dùng',
+                'description'=>'1',
+                'thumbnail'=>'http://babyborn.vn/Images/Product/ghe-nga-nam-an-cho-tre-so-sinh-to-toddler.jpg',
+            ],
+            [
+                'name'=>'Đồ chơi',
+                'description'=>'1',
+                'thumbnail'=>'https://media.bibomart.net/u/bbm/product/2015/08/25/17/48/410_410/360215_xe_3_banh_cho_be_1.jpg',
+            ],
+        ]);
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
