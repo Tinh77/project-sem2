f2d613c34906ff92676d71b91ae9c493d0c6657f
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-08-17 16:33:24 +0700

    Chinh sua giao dien phan home-page

--- resources/views/client/partial/slide.blade.php
+++ resources/views/client/partial/slide.blade.php
@@ -1,21 +1,47 @@
-<div class="simple-slider mb-4">
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide"
-                 style="background-image:url({{asset('image/19985205_1007040769398785_7801387006000365568_n.jpg')}});">
-                <div class="text-slide" style="left: 25%">Mọi thứ đều có ý nghĩa</div>
-            </div>
-            <div class="swiper-slide"
-                 style="background-image:url({{asset('image/19985547_502048723478998_1078989019905261568_n.jpg')}});">
-                {{--<div class="text-slide" style="right: 10%;bottom: 70%">Đừng bỏ đi đừng lãng phí hãy mang đến với chúng tôi</div>--}}
-            </div>
-            <div class="swiper-slide"
-                 style="background-image:url({{asset('https://scontent.fsgn2-2.fna.fbcdn.net/v/t1.15752-9/39132699_711285825930430_1694688954202193920_n.jpg?_nc_cat=0&oh=fc446c8d0708a6bc29fd4dcc38952bbb&oe=5C0C0602')}});">
-                <div class="text-slide">Đừng bỏ đi đừng lãng phí hãy mang đến với chúng tôi</div>
+<!--Carousel Wrapper-->
+<div id="carousel-example-2" class="carousel slide carousel-fade p-5" data-ride="carousel">
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-2" data-slide-to="1"></li>
+        <li data-target="#carousel-example-2" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <div class="view">
+                <div style="background-image: url({{asset('img/silde/19985205_1007040769398785_7801387006000365568_n.jpg')}});background-size: cover;background-position: center;background-repeat: no-repeat;width: auto;height: 600px"></div>
+                <div class=" mask rgba-black-light
+                ">
+                </div>
             </div>
         </div>
-        <div class="swiper-pagination"></div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
+        <div class="carousel-item">
+            <!--Mask color-->
+            <div class="view">
+                <div style="background-image: url({{asset('img/silde/20067115_1968554856709126_4894294574446411776_n.jpg')}});background-size: cover;background-position: center;background-repeat: no-repeat;width: auto;height: 600px"></div>
+                <div class="mask rgba-black-strong"></div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <!--Mask color-->
+            <div class="view">
+                <div style="background-image: url({{asset('img/silde/20066600_1916269608636822_3978446047578947584_n.jpg')}});background-size: cover;background-position: center;background-repeat: no-repeat;width: auto;height: 600px"></div>
+                <div class="mask rgba-black-slight"></div>
+            </div>
+        </div>
     </div>
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    <!--/.Controls-->
 </div>
+<!--/.Carousel Wrapper-->
