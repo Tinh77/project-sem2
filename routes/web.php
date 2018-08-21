03f0cfacb1eab97b6554af2740c337eceb902c24
exam <tuandpd00617@fpt.edu.vn>
2018-08-21 10:11:57 +0700

    sua seeder

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/admin/demo-form', function () {
     return view('admin.pages.form');
 });
 
-Route::resource('admin/demo', 'CategoryController');
+Route::resource('admin/category', 'CategoryController');
 
 Route::get('/admin/demo-list', function () {
     return view('admin.pages.table');
@@ -26,17 +26,25 @@ Route::get('/admin/demo-list', function () {
 
 Route::get('/client/demo-home','GiftController@indexHome');
 
-//Route::resource('/client/pages',"GiftController");
-Route::get('/client/pages/{id}', "GiftController@listCategory");
+Route::get('/client/pages/list',"GiftController@index");
+Route::get('/client/pages/list/{id}', "GiftController@listCategory");
+Route::get('/client/pages/{id}', "GiftController@show");
+
 Route::get('/client/demo-product-detail', function () {
     return view('client.pages.product-detail');
 });
-Route::get('/client/demo-gitf-form', function () {
-    return view('client.pages.gitfForm');
+Route::get('/client/gift/form', function () {
+    return view('client.pages.gift.form');
 });
+
+Route::get('/client/gift/list', function () {
+    return view('client.pages.gift.list');
+});
+
 Route::get('/client/404', function () {
     return view('client.404client.404');
 });
+
 Route::get('/admin/404', function () {
     return view('admin.404admin.404');
 });
