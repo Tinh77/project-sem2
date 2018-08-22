4eb94827df37cf4326565610429d6238b39d5e55
Giang Thị Tình <tinhgiang9898@gmail.com>
2018-08-22 22:04:24 +0700

    lay-transaction-ra-view

--- app/Account.php
+++ app/Account.php
@@ -13,5 +13,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
+    protected $table = 'accounts';
+    public $timestamps = true;
 
 }
---
+++ config/define.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GIANG TINH
+ * Date: 22/08/2018
+ * Time: 5:41 CH
+ */
+return [
+    'list_status' => [
+        0 => 'Chưa giao dịch',
+        1 => 'Đã giao dịch',
+        2 => 'Đang giao',
+        3 => 'Hoàn thành'
+    ],
+];
