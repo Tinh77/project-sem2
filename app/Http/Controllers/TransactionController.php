47886dce6c40327dbf847ccdcbc15633b80f0078
Giang Thị Tình <tinhgiang9898@gmail.com>
2018-09-06 08:39:51 +0700

    them-phan-js-phan-loc-theo-tuoi

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -24,15 +24,11 @@ class TransactionController extends Controller
                 ->join('accounts', 'transactions.buyer_id', '=', 'accounts.id')
                 ->select('transactions.*', 'gifts.images as gift_images', 'gifts.name as gift_name', 'accounts.first_name as account_first_name')
                 ->get();
-
-
             $obj_buyer_id = Transaction::where('buyer_id', '=', $account_id)
                 ->join('gifts', 'transactions.gift_id', '=', 'gifts.id')
                 ->join('accounts', 'transactions.buyer_id', '=', 'accounts.id')
                 ->select('transactions.*', 'gifts.images as gift_images', 'gifts.name as gift_name', 'accounts.first_name as account_first_name')
                 ->get();
-
-
             return view('client.pages.gift.listGift')->with(['obj_owner_id' => $obj_owner_id, 'obj_buyer_id' => $obj_buyer_id]);
         } else {
             return redirect('/login');
